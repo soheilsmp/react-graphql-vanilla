a078d4beabaee1879fd7791cecb0680ba27d1ca3
Soheil <smp@localhost.localdomain>
2018-11-09 19:46:17 +0330

    graphql query

--- src/App.js
+++ src/App.js
@@ -1,28 +1,130 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios  from 'axios';
+
 import './App.css';
 
+const title = 'React Graphql Github Client';
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
+
+const GET_REPOSITORY_OF_ORGANIZATION = `
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+      repository(name: "the-road-to-learn-react") {
+        name
+        url
+      }
+    }
+  }
+`;
+
 class App extends Component {
+  state={
+    path: 'the-road-to-lear-react/the-road-to-learn-react',
+    organization: null,
+    errors: null,
+  };
+
+  componentDidMount() {
+    this.onFetchFromGitHub();
+  }
+
+  onChange = event => {
+    this.setState({ path: event.target.value });
+  };
+
+  onSubmit = event => {
+    // fetch data
+
+    event.preventDefault();
+  };
+
+  onFetchFromGitHub = () => {
+    axiosGitHubGraphQL
+      .post('', { query: GET_REPOSITORY_OF_ORGANIZATION })
+      .then(result => {
+        console.log(result.data);
+        this.setState(() => ({
+          organization: result.data.data.organization,
+          errors: result.data.errors,
+        }));
+      }
+      );
+  }
+
   render() {
+    const { path, organization, errors } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{title}</h1>
+
+        <form onSubmit={this.onSubmit}>
+           <label htmlFor="url">
+            Show open issues for https://github.com/
+          </label>
+          <input
+            id="url"
+            type="text"
+            value={path}
+            onChange={this.onChange}
+            style={{ width: '300px' }}
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        <hr />
+
+        { organization ? (
+          <Organization organization={organization} errors={errors} />
+        ) : (
+          <p>No information yet ...</p>
+        )}
+
       </div>
     );
   }
 }
+
+const Organization = ({ organization, errors }) => {
+  if (errors) {
+    return(
+      <p>
+        <strong>Something went wrong:</strong>
+        {errors.map(error => error.message).join(' ')}
+      </p>
+    );
+  }
+
+  console.log(organization.repository.url);
+
+  return (
+    <div>
+      <p>
+        <strong>Issues from Organization:</strong>
+        <a href={organization.url}>{organization.name}</a>
+      </p>
+      <Repository repository={organization.repository} />
+    </div>
+  );
+};
+
+const Repository = ({repository}) => (
+  <div>
+    <p>
+      <strong>In Repository:</strong>
+      <a href={repository.url}>{repository.name}</a>
+    </p>
+  </div>
+);
 
 export default App;
